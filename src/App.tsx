7721b309fe6fb9a256a5a97641b0efec0e700610
alejandro-cardenas-g <alejandro.cardenas.g07@gmail.com>
2023-07-28 17:52:18 -0500

    bases

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ColorChanger } from "./components/colorChanger";
+
+function App() {
+  return (
+    <div>
+      <ColorChanger text="texto color" />
+    </div>
+  );
+}
+
+export default App;
